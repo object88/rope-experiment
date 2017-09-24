de9fa748331630d6844e2b48bbf6588ddae9fd07
object88 <object88@gmail.com>
2017-09-23 20:43:34 -0600

    Minor test update

--- rope_test.go
+++ rope_test.go
@@ -12,36 +12,18 @@ var version = flag.String("ver", "", "version of rope")
 
 type ropeCreator func(init string) Rope
 
-func Benchmark_Add_Small(b *testing.B) {
-	tests := []struct {
-		name string
-		init string
-	}{
-		{"1000", RandStringBytesMaskImprSrc(1000)},
-		{"2500", RandStringBytesMaskImprSrc(2500)},
-		{"5000", RandStringBytesMaskImprSrc(5000)},
-		{"7500", RandStringBytesMaskImprSrc(7500)},
-		{"10000", RandStringBytesMaskImprSrc(10000)},
-		{"12500", RandStringBytesMaskImprSrc(12500)},
-		{"15000", RandStringBytesMaskImprSrc(15000)},
-		// {"17500", RandStringBytesMaskImprSrc(17500)},
-		// {"20000", RandStringBytesMaskImprSrc(20000)},
-	}
+func Test_Add(t *testing.T) {
+	init := RandStringBytesMaskImprSrc(1000)
+	r := CreateV2(init)
 
-	var rc ropeCreator
-	if *version == "1" {
-		rc = func(init string) Rope { return CreateV1(init) }
-	} else if *version == "2" {
-		rc = func(init string) Rope { return CreateV2(init) }
-	} else {
-		b.Fatalf("'%s' is not a valid version", *version)
-	}
+	r.Insert(0, "a")
 
-	for _, tc := range tests {
-		testAdd(rc, tc.name, tc.init, b)
+	result := r.String()
+	expected := "a" + init
+	if result != expected {
+		t.Fatalf("Insert failed:\nExpected:\n'%s'\nGet:\n'%s'", init, result)
 	}
 }
-
 func Test_Reader(t *testing.T) {
 	init := RandStringBytesMaskImprSrc(1000)
 
@@ -56,6 +38,47 @@ func Test_Reader(t *testing.T) {
 	result := string(buf.Bytes())
 	if strings.Compare(result, init) != 0 {
 		t.Fatalf("Read failed:\nExpected:\n'%s'\nGot:\n'%s'", init, result)
+	}
+}
+
+func Test_Remove(t *testing.T) {
+	init := RandStringBytesMaskImprSrc(1000)
+	r := CreateV2(init)
+
+	r.Remove(0, 1)
+
+	result := r.String()
+	expected := init[1:]
+	if result != expected {
+		t.Fatalf("Remove failed:\nExpected:\n'%s'\nGet:\n'%s'", init, result)
+	}
+}
+
+func Benchmark_Add_Small(b *testing.B) {
+	tests := []struct {
+		name string
+		init string
+	}{
+		{"1000", RandStringBytesMaskImprSrc(1000)},
+		{"2500", RandStringBytesMaskImprSrc(2500)},
+		{"5000", RandStringBytesMaskImprSrc(5000)},
+		{"7500", RandStringBytesMaskImprSrc(7500)},
+		{"10000", RandStringBytesMaskImprSrc(10000)},
+		{"12500", RandStringBytesMaskImprSrc(12500)},
+		{"15000", RandStringBytesMaskImprSrc(15000)},
+	}
+
+	var rc ropeCreator
+	if *version == "1" {
+		rc = func(init string) Rope { return CreateV1(init) }
+	} else if *version == "2" {
+		rc = func(init string) Rope { return CreateV2(init) }
+	} else {
+		b.Fatalf("'%s' is not a valid version", *version)
+	}
+
+	for _, tc := range tests {
+		testAdd(rc, tc.name, tc.init, b)
 	}
 }
 
